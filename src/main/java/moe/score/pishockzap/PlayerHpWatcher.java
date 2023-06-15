305d1067d17065e391b30482efc848c7cb2baa82
Score_Under <seejay.11@gmail.com>
2023-06-15 22:51:30 +0100

    Ignore one more player HP update per world change
    Also, remove that vanishingly-small chance that the player will
    have the same identity hash code between two different instances with
    non-overlapping lifetimes.
    
    Bump to 1.0.1

--- src/main/java/moe/score/pishockzap/PlayerHpWatcher.java
+++ src/main/java/moe/score/pishockzap/PlayerHpWatcher.java
@@ -2,14 +2,26 @@ package moe.score.pishockzap;
 
 import net.minecraft.entity.LivingEntity;
 
+import java.lang.ref.WeakReference;
+
 public class PlayerHpWatcher {
-    private int lastPlayer = -1;
+    private static final WeakReference<LivingEntity> NULL_WEAK_REFERENCE = new WeakReference<>(null);
+    private WeakReference<LivingEntity> lastPlayer = NULL_WEAK_REFERENCE;
     private int lastPlayerHp = -1;
+    private int ignore = 0;
 
     public int updatePlayerHpAndGetDamage(LivingEntity player, int hp) {
-        int playerId = System.identityHashCode(player);
-        if (playerId != lastPlayer) {
-            lastPlayer = playerId;
+        if (player != lastPlayer.get()) {
+            lastPlayer = new WeakReference<>(player);
+            lastPlayerHp = hp;
+            // Ignore the first update after the player is changed
+            // because loading into a world will trigger 2 updates
+            ignore = 1;
+            return 0;
+        }
+
+        if (ignore != 0) {
+            ignore--;
             lastPlayerHp = hp;
             return 0;
         }
@@ -20,7 +32,7 @@ public class PlayerHpWatcher {
     }
 
     public void resetPlayer() {
-        lastPlayer = -1;
+        lastPlayer = NULL_WEAK_REFERENCE;
         lastPlayerHp = -1;
     }
 }
