5784a79bd56257ad5f5a2e10303994b741cc22ca
Score_Under <seejay.11@gmail.com>
2026-04-21 10:14:04 +0100

    Add mc1.19 and mc1.19.3 series

---
+++ compat/buttons_predrawcontext/moe/score/pishockzap/compat/ButtonListEntry.java
@@ -0,0 +1,73 @@
+package moe.score.pishockzap.compat;
+
+import lombok.Builder;
+import me.shedaniel.clothconfig2.gui.entries.TooltipListEntry;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.Selectable;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.util.Window;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class ButtonListEntry extends TooltipListEntry<Void> {
+    private final ButtonWidget buttonWidget;
+    private final List<ClickableWidget> widgets;
+
+    @SuppressWarnings({"deprecation", "UnstableApiUsage"})
+    @Builder(setterPrefix = "set")
+    public ButtonListEntry(Text fieldName, Text buttonText, Consumer<ButtonListEntry> onClickCallback, Supplier<Optional<Text[]>> tooltipSupplier) {
+        super(fieldName, tooltipSupplier);
+        this.buttonWidget = ButtonWidget.builder(buttonText, btn -> onClickCallback.accept(this))
+                .dimensions(0, 0, 150, 20).build();
+        this.widgets = List.of(buttonWidget);
+    }
+
+    public void setButtonText(Text text) {
+        buttonWidget.setMessage(text);
+    }
+
+    public boolean isEdited() {
+        return false;
+    }
+
+    public Void getValue() {
+        return null;
+    }
+
+    public Optional<Void> getDefaultValue() {
+        return Optional.empty();
+    }
+
+    public void render(MatrixStack matrices, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean isHovered, float delta) {
+        super.render(matrices, index, y, x, entryWidth, entryHeight, mouseX, mouseY, isHovered, delta);
+        Window window = MinecraftClient.getInstance().getWindow();
+        buttonWidget.active = isEditable();
+        buttonWidget.setY(y);
+        var displayed = getDisplayedFieldName();
+        var textRenderer = MinecraftClient.getInstance().textRenderer;
+        if (textRenderer.isRightToLeft()) {
+            textRenderer.drawWithShadow(matrices, displayed.asOrderedText(), window.getScaledWidth() - x - textRenderer.getWidth(displayed), y + 6, 0xffffff);
+            buttonWidget.setX(x);
+        } else {
+            textRenderer.drawWithShadow(matrices, displayed.asOrderedText(), x, y + 6, this.getPreferredTextColor());
+            buttonWidget.setX(x + entryWidth - 150);
+        }
+
+        buttonWidget.render(matrices, mouseX, mouseY, delta);
+    }
+
+    public List<? extends Element> children() {
+        return widgets;
+    }
+
+    public List<? extends Selectable> narratables() {
+        return widgets;
+    }
+}
