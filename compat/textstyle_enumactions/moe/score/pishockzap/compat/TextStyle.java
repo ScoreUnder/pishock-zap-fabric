a8249936ae309605ecd01d6ce7b210a78e4c78f6
Score_Under <seejay.11@gmail.com>
2026-04-21 08:53:46 +0100

    Add mc1.21

--- compat/textstyle_enumactions/moe/score/pishockzap/compat/TextStyle.java
+++ compat/textstyle_enumactions/moe/score/pishockzap/compat/TextStyle.java
@@ -2,13 +2,13 @@ package moe.score.pishockzap.compat;
 
 import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Style;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.Text;
 
 public class TextStyle {
     private TextStyle() {
     }
 
-    public static Style setHoverText(Style style, TranslatableText text) {
+    public static Style setHoverText(Style style, Text text) {
         return style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, text));
     }
 }
