b341e0284eae50e603812157075b7dcfea5fe691
Score_Under <seejay.11@gmail.com>
2026-07-20 11:35:13 +0100

    Use phantom reference in PlayerHpWatcher
    This means there's no way to get() the Player accidentally

--- common/src/main/java/moe/score/pishockzap/PlayerHpWatcher.java
+++ common/src/main/java/moe/score/pishockzap/PlayerHpWatcher.java
@@ -3,13 +3,14 @@ package moe.score.pishockzap;
 import lombok.NonNull;
 import org.jetbrains.annotations.ApiStatus;
 
-import java.lang.ref.WeakReference;
+import java.lang.ref.PhantomReference;
 
 @ApiStatus.Internal
 public class PlayerHpWatcher<T> {
-    private static final WeakReference<Object> NULL_WEAK_REFERENCE = new WeakReference<>(null);
+    private static final PhantomReference<Object> NULL_PHANTOM_REFERENCE = new PhantomReference<>(null, null);
+    // Player kept as a PhantomReference to prevent accidental retrieval of a stale Player object.
     @SuppressWarnings("unchecked")
-    private @NonNull WeakReference<T> lastPlayer = (WeakReference<T>) NULL_WEAK_REFERENCE;
+    private @NonNull PhantomReference<T> lastPlayer = (PhantomReference<T>) NULL_PHANTOM_REFERENCE;
     private float lastPlayerHp = -1;
     private int ignore = 0;
 
@@ -24,8 +25,8 @@ public class PlayerHpWatcher<T> {
     }
 
     private float calculateDamage(@NonNull T player, float hp) {
-        if (player != lastPlayer.get()) {
-            lastPlayer = new WeakReference<>(player);
+        if (!lastPlayer.refersTo(player)) {
+            lastPlayer = new PhantomReference<>(player, null);
             // Ignore the first update after the player is changed
             // because loading into a world will trigger 2 updates
             ignore = 1;
@@ -42,7 +43,7 @@ public class PlayerHpWatcher<T> {
 
     @SuppressWarnings("unchecked")
     public void resetPlayer() {
-        lastPlayer = (WeakReference<T>) NULL_WEAK_REFERENCE;
+        lastPlayer = (PhantomReference<T>) NULL_PHANTOM_REFERENCE;
         lastPlayerHp = -1;
     }
 }
